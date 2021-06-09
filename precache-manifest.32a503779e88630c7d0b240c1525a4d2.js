b77fea134cc0e7047b77a3f6e6bcc882bb6c4d1c
AshutoshMishra22 <63530972+AshutoshMishra22@users.noreply.github.com>
2021-06-09 18:28:01 +0530

    Updates

---
+++ precache-manifest.32a503779e88630c7d0b240c1525a4d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf1e0de6332f033189bed3e48e256be",
+    "url": "/ImageDetection/index.html"
+  },
+  {
+    "revision": "a28b439fbac9ed54cdc8",
+    "url": "/ImageDetection/static/css/main.420b58aa.chunk.css"
+  },
+  {
+    "revision": "fccdc84140d1508924e9",
+    "url": "/ImageDetection/static/js/2.53bf27ba.chunk.js"
+  },
+  {
+    "revision": "56050d2b133b79baa00050003b160322",
+    "url": "/ImageDetection/static/js/2.53bf27ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a28b439fbac9ed54cdc8",
+    "url": "/ImageDetection/static/js/main.a72a242e.chunk.js"
+  },
+  {
+    "revision": "b3f89c262eb44d9596fe",
+    "url": "/ImageDetection/static/js/runtime-main.a40cc5be.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ImageDetection/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d05ff73b232424ca72790f2187b00894",
+    "url": "/ImageDetection/static/media/spinner.d05ff73b.gif"
+  }
+]);
